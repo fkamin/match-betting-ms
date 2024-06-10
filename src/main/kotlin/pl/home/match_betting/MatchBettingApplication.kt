8f6a341c674ceed8c50e39447dfdf5dec1ca2661
Filip Kaminski <s95429@pollub.edu.pl>
2024-06-10 21:41:07 +0200

    Basic Setup

---
+++ src/main/kotlin/pl/home/match_betting/MatchBettingApplication.kt
@@ -0,0 +1,11 @@
+package pl.home.match_betting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatchBettingApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatchBettingApplication>(*args)
+}
---
+++ src/test/kotlin/pl/home/match_betting/MatchBettingApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.home.match_betting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatchBettingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
